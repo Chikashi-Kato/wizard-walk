63d0968cc8fee94a9a0cfc5337ee4e353b17b45a
Chuck Kato <chuck.kato@coursehero.com>
2021-09-04 18:55:25 -0700

    Add the opening logo

--- main.go
+++ main.go
@@ -11,8 +11,15 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
+type gameState string
+
 const (
-	MOVEMENT = 5
+	LogoSpeed = 0.005
+	LogoWait = 60 // frame
+	WalkSpeed = 5
+
+	LogoState = gameState("logo")
+	PlayState = gameState("play")
 )
 
 type size struct{
@@ -39,69 +46,99 @@ type World struct {
 	loc location
 }
 
+type Logo struct {
+	logo     *ebiten.Image
+	scale    float64
+	duration int // frame
+}
+
 type Game struct{
+	state gameState
 	world World
+	logo Logo
 
 	windowSize size
 	wizard *Wizard
 }
 
 func (g *Game) Update() error {
-	for _, p := range inpututil.PressedKeys() {
-		switch p {
-		case ebiten.KeyArrowUp:
-			if g.wizard.loc.y > 0 {
-				if g.wizard.loc.y <= g.getWindowCenter().y && g.world.loc.y == 0 ||
-					(g.wizard.loc.y > g.getWindowCenter().y && g.world.loc.y >= g.world.worldSize.height - g.windowSize.height) {
-					g.wizard.loc.y -= MOVEMENT
-				} else if g.world.loc.y > 0 {
-					g.world.loc.y -= MOVEMENT
+	switch g.state {
+	case PlayState:
+		for _, p := range inpututil.PressedKeys() {
+			switch p {
+			case ebiten.KeyArrowUp:
+				if g.wizard.loc.y > 0 {
+					if g.wizard.loc.y <= g.getWindowCenter().y && g.world.loc.y == 0 ||
+						(g.wizard.loc.y > g.getWindowCenter().y && g.world.loc.y >= g.world.worldSize.height-g.windowSize.height) {
+						g.wizard.loc.y -= WalkSpeed
+					} else if g.world.loc.y > 0 {
+						g.world.loc.y -= WalkSpeed
+					}
 				}
-			}
-		case ebiten.KeyArrowLeft:
-			if g.wizard.loc.x > 0 {
-				if g.wizard.loc.x <= g.getWindowCenter().x && g.world.loc.x == 0 ||
-					(g.wizard.loc.x > g.getWindowCenter().x && g.world.loc.x >= g.world.worldSize.width - g.windowSize.width){
-					g.wizard.loc.x -= MOVEMENT
-				} else if g.world.loc.x > 0 {
-					g.world.loc.x -= MOVEMENT
+			case ebiten.KeyArrowLeft:
+				if g.wizard.loc.x > 0 {
+					if g.wizard.loc.x <= g.getWindowCenter().x && g.world.loc.x == 0 ||
+						(g.wizard.loc.x > g.getWindowCenter().x && g.world.loc.x >= g.world.worldSize.width-g.windowSize.width) {
+						g.wizard.loc.x -= WalkSpeed
+					} else if g.world.loc.x > 0 {
+						g.world.loc.x -= WalkSpeed
+					}
 				}
-			}
-		case ebiten.KeyArrowDown:
-			if g.wizard.loc.y < g.world.worldSize.height {
-				if g.wizard.loc.y < g.getWindowCenter().y && g.world.loc.y == 0 ||
-					(g.world.loc.y >= g.world.worldSize.height-g.windowSize.height && g.wizard.loc.y < g.windowSize.height - 75) {
-					g.wizard.loc.y += MOVEMENT
-				} else if g.world.loc.y < g.world.worldSize.height-g.windowSize.height {
-					g.world.loc.y += MOVEMENT
+			case ebiten.KeyArrowDown:
+				if g.wizard.loc.y < g.world.worldSize.height {
+					if g.wizard.loc.y < g.getWindowCenter().y && g.world.loc.y == 0 ||
+						(g.world.loc.y >= g.world.worldSize.height-g.windowSize.height && g.wizard.loc.y < g.windowSize.height-75) {
+						g.wizard.loc.y += WalkSpeed
+					} else if g.world.loc.y < g.world.worldSize.height-g.windowSize.height {
+						g.world.loc.y += WalkSpeed
+					}
 				}
-			}
-		case ebiten.KeyArrowRight:
-			if g.wizard.loc.x < g.getWindowCenter().x ||
-				(g.world.loc.x >= g.world.worldSize.width - g.windowSize.width && g.wizard.loc.x < g.windowSize.width - 75) {
-				g.wizard.loc.x += MOVEMENT
-			} else if g.world.loc.x < g.world.worldSize.width - g.windowSize.width {
-				g.world.loc.x += MOVEMENT
+			case ebiten.KeyArrowRight:
+				if g.wizard.loc.x < g.getWindowCenter().x ||
+					(g.world.loc.x >= g.world.worldSize.width-g.windowSize.width && g.wizard.loc.x < g.windowSize.width-75) {
+					g.wizard.loc.x += WalkSpeed
+				} else if g.world.loc.x < g.world.worldSize.width-g.windowSize.width {
+					g.world.loc.x += WalkSpeed
+				}
 			}
 		}
+		fmt.Println(g.wizard.loc.x, g.wizard.loc.y, g.world.loc.x, g.world.loc.y, g.getWindowCenter().x, g.getWindowCenter().y)
 	}
-	fmt.Println(g.wizard.loc.x, g.wizard.loc.y, g.world.loc.x, g.world.loc.y, g.getWindowCenter().x, g.getWindowCenter().y)
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	switch g.state {
+	case PlayState:
+		croppedMap := g.world.background.SubImage(image.Rectangle{
+			Min: image.Point{g.world.loc.x, g.world.loc.y},
+			Max: image.Point{g.world.loc.x + g.windowSize.width, g.world.loc.y + g.windowSize.height},
+		}).(*ebiten.Image)
+		screen.DrawImage(croppedMap, nil)
 
-	croppedMap := g.world.background.SubImage(image.Rectangle{
-		Min: image.Point{g.world.loc.x, g.world.loc.y},
-		Max: image.Point{g.world.loc.x + g.windowSize.width, g.world.loc.y + g.windowSize.height},
-	}).(*ebiten.Image)
-	screen.DrawImage(croppedMap, nil)
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(1.5, 1.5)
+		op.GeoM.Translate(float64(g.wizard.loc.x), float64(g.wizard.loc.y))
+		screen.DrawImage(g.wizard.left, op)
+	case LogoState:
+		op := &ebiten.DrawImageOptions{}
+		x, y := g.logo.logo.Size()
+		x = g.windowSize.width / 2 - x / 2
+		y = g.windowSize.height / 2 - y / 2
+		op.GeoM.Translate(float64(x), float64(y))
 
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(1.5, 1.5)
-	op.GeoM.Translate(float64(g.wizard.loc.x), float64(g.wizard.loc.y))
-	screen.DrawImage(g.wizard.left, op)
+		if g.logo.scale < 1 {
+			g.logo.scale += LogoSpeed
+			op.ColorM.Scale(g.logo.scale, g.logo.scale, g.logo.scale, 1)
+		} else if g.logo.duration < LogoWait {
+			g.logo.duration += 1
+		} else {
+			g.state = PlayState
+		}
+
+		screen.DrawImage(g.logo.logo, op)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -116,6 +153,7 @@ func main() {
 	var err error
 
 	g := &Game{}
+	g.state = LogoState
 	g.windowSize = size{640, 480}
 
 	ebiten.SetWindowSize(g.windowSize.width,g.windowSize.height)
@@ -140,6 +178,13 @@ func main() {
 		left: wiz,
 		down: wiz,
 		right: wiz,
+	}
+
+	// Load logo
+	g.logo.logo, _, err = ebitenutil.NewImageFromFile("./images/forgotten-runes-logo.png")
+
+	if err != nil {
+		log.Fatal("logo file not found")
 	}
 
 	if err := ebiten.RunGame(g); err != nil {
